4c18ea4df046c03d8331f4d7d3356356c62eae45
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2023-03-02 13:24:11 +0700

    Update database.py

--- config/database.py
+++ config/database.py
@@ -5,7 +5,6 @@ from core import config
 ssl._create_default_https_context = ssl._create_unverified_context
 
 client = MongoClient(f'mongodb+srv://{config.settings.username}:{config.settings.password}@{config.settings.host}/test')
-# client = MongoClient('mongodb+srv://root:1234@cluster0.bdrbewv.mongodb.net/test')
 
 db = client.swe_classroom
 
